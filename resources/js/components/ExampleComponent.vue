541733095ce851889e98de43a81ba1df3049111e
Shamshod <abdushamshod@gmail.com>
2021-08-06 16:43:46 +0500

    commit

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,60 +1,73 @@
 <template>
-   <div id="app contact m-auto">
-       <div class="container">
-           <h3 class=" for-bottom main_title">Contact</h3>
-           <div class="row col-7 m-auto">
-                <form action="" >
+    <div id="app contact m-auto">
+        <div class="container">
+            <h3 class=" for-bottom main_title">Contact</h3>
+            <div class="row col-7 m-auto">
+                <form action="">
                     <div class="forms">
                         <label class="label" for="email">Email</label>
-                        <input class="for-bottom" type="email" name="" id="email">
+                        <input
+                            class="for-bottom"
+                            type="email"
+                            name=""
+                            id="email"
+                        />
                         <label class="label" for="text">Some text</label>
-                        <textarea class="for-bottom" name="" id="text" cols="30" rows="10" ></textarea>
-                        <button class="btn-success btn m-auto" type="submit">Send Message</button>
+                        <textarea
+                            class="for-bottom"
+                            name=""
+                            id="text"
+                            cols="30"
+                            rows="10"
+                        ></textarea>
+                        <button
+                            class="btn-success btn m-auto for-bottom"
+                            type="submit"
+                        >
+                            Send Message
+                        </button>
                     </div>
-
-                 </form>
-           </div>
-       </div>
-
-
-   </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+    mounted() {
+        console.log("Component mounted.");
     }
+};
 </script>
+
 <style scoped>
-.forms{
+.forms {
     display: flex;
-    flex-direction: column ;
+    flex-direction: column;
     margin: auto;
 }
 
-
-.for-bottom{
-    margin-bottom: 15px;
+.for-bottom {
+    margin-bottom: 15px !important;
 }
 
-.m-auto{
+.m-auto {
     margin: auto;
     display: block;
 }
 
-.label{
+.label {
     color: #00a85a;
 }
 
-.main_title{
+.main_title {
     display: block;
     text-align: center;
     margin-top: 45px;
 }
 
-.btn{
+.btn {
     width: 150px;
 }
 </style>
